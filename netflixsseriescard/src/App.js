3ecc8e75c194696bc9a9bfe82bebdf922c97e273
AbhishekRanjan8051 <abhishekranjan8051@gmail.com>
2021-11-05 20:42:09 +0530

    Using :- map function

--- netflixsseriescard/src/App.js
+++ netflixsseriescard/src/App.js
@@ -1,47 +1,20 @@
 import "./App.css";
 import { Netflix } from "./components/Netflix";
+import { sdata } from "./components/Card";
+
+function cardshow(val) {
+  return (
+    <Netflix
+      image={val.image}
+      title={val.title}
+      sname={val.sname}
+      link={val.link}
+    />
+  );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <Netflix
-        image="https://filmdaily.co/wp-content/uploads/2020/06/moneyheist-lede-5-1300x650.jpg"
-        title="A Netflix Original Series"
-        sname="Money Heist"
-        link="https://www.netflix.com/in/title/80192098"
-      />
-      <Netflix
-        image="https://filmdaily.co/wp-content/uploads/2020/06/moneyheist-lede-5-1300x650.jpg"
-        title="A Netflix Original Series"
-        sname="Money Heist"
-        link="https://www.netflix.com/in/title/80192098"
-      />
-      <Netflix
-        image="https://filmdaily.co/wp-content/uploads/2020/06/moneyheist-lede-5-1300x650.jpg"
-        title="A Netflix Original Series"
-        sname="Money Heist"
-        link="https://www.netflix.com/in/title/80192098"
-      />
-      <Netflix
-        image="https://akm-img-a-in.tosshub.com/indiatoday/images/story/202006/qc_12.jpeg?.Pyl3OuYjfGDzyEqSOXmK38YKttjbT3s&size=770:433"
-        title="A Netflix Original Series"
-        sname="Dark"
-        link="https://www.netflix.com/in/title/80100172"
-      />
-      <Netflix
-        image="https://akm-img-a-in.tosshub.com/indiatoday/images/story/202006/qc_12.jpeg?.Pyl3OuYjfGDzyEqSOXmK38YKttjbT3s&size=770:433"
-        title="A Netflix Original Series"
-        sname="Dark"
-        link="https://www.netflix.com/in/title/80100172"
-      />
-      <Netflix
-        image="https://akm-img-a-in.tosshub.com/indiatoday/images/story/202006/qc_12.jpeg?.Pyl3OuYjfGDzyEqSOXmK38YKttjbT3s&size=770:433"
-        title="A Netflix Original Series"
-        sname="Dark"
-        link="https://www.netflix.com/in/title/80100172"
-      />
-    </div>
-  );
+  return <div className="App">{sdata.map(cardshow)}</div>;
 }
 
 export default App;
---
+++ netflixsseriescard/src/components/Card.jsx
@@ -0,0 +1,45 @@
+const sdata = [
+  {
+    image:
+      "https://filmdaily.co/wp-content/uploads/2020/06/moneyheist-lede-5-1300x650.jpg",
+    title: "A Netflix Original Series",
+    sname: "Money Heist",
+    link: "https://www.netflix.com/in/title/80192098",
+  },
+  {
+    image:
+      "https://akm-img-a-in.tosshub.com/indiatoday/images/story/202006/qc_12.jpeg?.Pyl3OuYjfGDzyEqSOXmK38YKttjbT3s&size=770:433",
+    title: "A Netflix Original Series",
+    sname: "Dark",
+    link: "https://www.netflix.com/in/title/80100172",
+  },
+  {
+    image:
+      "https://filmdaily.co/wp-content/uploads/2020/06/moneyheist-lede-5-1300x650.jpg",
+    title: "A Netflix Original Series",
+    sname: "Money Heist",
+    link: "https://www.netflix.com/in/title/80192098",
+  },
+  {
+    image:
+      "https://akm-img-a-in.tosshub.com/indiatoday/images/story/202006/qc_12.jpeg?.Pyl3OuYjfGDzyEqSOXmK38YKttjbT3s&size=770:433",
+    title: "A Netflix Original Series",
+    sname: "Dark",
+    link: "https://www.netflix.com/in/title/80100172",
+  },
+  {
+    image:
+      "https://filmdaily.co/wp-content/uploads/2020/06/moneyheist-lede-5-1300x650.jpg",
+    title: "A Netflix Original Series",
+    sname: "Money Heist",
+    link: "https://www.netflix.com/in/title/80192098",
+  },
+  {
+    image:
+      "https://akm-img-a-in.tosshub.com/indiatoday/images/story/202006/qc_12.jpeg?.Pyl3OuYjfGDzyEqSOXmK38YKttjbT3s&size=770:433",
+    title: "A Netflix Original Series",
+    sname: "Dark",
+    link: "https://www.netflix.com/in/title/80100172",
+  },
+];
+export { sdata };
