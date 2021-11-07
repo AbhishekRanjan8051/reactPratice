77eae2e27b051cf6db0ca7c06f399abc736f5d2b
AbhishekRanjan8051 <abhishekranjan8051@gmail.com>
2021-11-07 15:50:45 +0530

    card

--- netflixsseriescard/src/components/Card.jsx
+++ netflixsseriescard/src/components/Card.jsx
@@ -41,5 +41,13 @@ const sdata = [
     sname: "Dark",
     link: "https://www.netflix.com/in/title/80100172",
   },
+  {
+    image:
+      "https://filmdaily.co/wp-content/uploads/2020/06/moneyheist-lede-5-1300x650.jpg",
+    title: "A Netflix Original Series",
+    sname: "Money Heist",
+    link: "https://www.netflix.com/in/title/80192098",
+  },
+  
 ];
 export { sdata };
