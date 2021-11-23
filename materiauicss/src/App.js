b8061e0d41d4130dd4b32ea9288f1ff6babf7300
AbhishekRanjan8051 <abhishekranjan8051@gmail.com>
2021-11-23 17:52:53 +0530

    bacha hua

---
+++ materiauicss/src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import { BasicButtons } from './components/BasicButtons';
+
+
+function App() {
+  return (
+    <div className="App">
+    <BasicButtons></BasicButtons>
+    </div>
+  );
+}
+
+export default App;
--- netflixsseriescard/src/App.js
+++ netflixsseriescard/src/App.js
@@ -2,17 +2,6 @@ import "./App.css";
 import { Netflix } from "./components/Netflix";
 import { sdata } from "./components/Card";
 
-// function cardshow(val) {
-//   return (
-//     <Netflix
-//       image={val.image}
-//       title={val.title}
-//       sname={val.sname}
-//       link={val.link}
-//     />
-//   );
-// }
-
 function App() {
   return (
     <div className="App">
